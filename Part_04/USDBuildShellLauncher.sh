23c68bf587d4d4ce3c39c824240156dcdc0390a3
carlynorama <carlynorama@gmail.com>
2023-06-30 14:16:30 -0700

    document scripts pt 1

---
+++ Part_04/USDBuildShellLauncher.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## uses pyenv to control version of python
+## after installing, run pyenv init in prefered
+## shell for instructions to finish configuration.
+export PYENV_VERSION=3.7.9
+
+BASEPATH=$(dirname "$0")
+export PATH=$PATH:$BASEPATH/bin;
+export PYTHONPATH=$PYTHONPATH:$BASEPATH/lib/python
+
+$SHELL
---
+++ Part_04/use379_only.command
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+## uses pyenv to control version of python
+## after installing, run pyenv init in prefered
+## shell for instructions to finish configuration.
+export PYENV_VERSION=3.7.9
+
+$SHELL
