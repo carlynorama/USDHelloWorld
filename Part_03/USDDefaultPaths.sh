0334f5d59309a8958acc9dfcca6b22539bb1e543
carlynorama <carlynorama@gmail.com>
2023-06-29 17:36:52 -0700

    Create USDDefaultPaths.sh

---
+++ Part_03/USDDefaultPaths.sh
@@ -0,0 +1,6 @@
+# usage in pwd with script
+# . ./USDDefaultPaths.sh 
+# usage if not in pwd with script
+# . /Path/To/Script/USDDefaultPaths.sh 
+export PATH=${PATH}:$HOME/opb/USDefault/bin
+export PYTHONPATH=${PYTHONPATH}:$HOME/opb/USDefault/lib/python
