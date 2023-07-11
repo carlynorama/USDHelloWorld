aeefdd5968a60530e460e963f414f7d6442cb580
carlynorama <carlynorama@gmail.com>
2023-07-10 18:20:32 -0700

    working smash!

--- Part_07/smashusds.sh
+++ Part_07/smashusds.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #PATH_TO_USDCAT="somepath"
-REMOVE_TMP_FLAG=1
+REMOVE_TMP_FLAG=false
 TMP_FILE_NAME="smash_tmp.usda"
 OUTPUT_NAME="smash_output.usdc"
 
 
-usdcat --flatten $@ > $TMP_FILE_NAME
+usdcat --flattenLayerStack $@ > $TMP_FILE_NAME
 usdcat -o $OUTPUT_NAME $TMP_FILE_NAME
-if REMOVE_TMP_FLAG; then
+if $REMOVE_TMP_FLAG; then
     rm $TMP_FILE_NAME
 fi
